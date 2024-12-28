2803d7d519f5501ff25854dbb76bef733be142a5
Ömer Apaydın <omerapaydin90@gmail.com>
2024-12-28 20:57:36 +0300

    First

--- LinkButtonUI/ContentView.swift
+++ LinkButtonUI/ContentView.swift
@@ -10,10 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Link("Google",destination: URL(string: "https://www.google.com")!)
+                .buttonStyle(.borderless)
+                .padding()
+            Link("Twitter",destination: URL(string: "https://www.twitter.com")!)
+                .buttonStyle(.bordered)
+                .buttonBorderShape(.roundedRectangle)
+                .controlSize(.regular)
+                .padding()
+            
+            Link("Send an Email",destination: URL(string: "omerapaydin@gmail.com")!)
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+                .tint(.pink)
+                .padding()
+            Link("Credo Academy",destination: URL(string: "www.credoacademy.com")!)
+                .buttonStyle(.plain)
+                .padding()
+                .border(.primary,width: 2)
         }
         .padding()
     }
