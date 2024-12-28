2fe6896718b543a0475d43bcaacc6eaefc681f46
Ömer Apaydın <omerapaydin90@gmail.com>
2024-12-28 21:07:16 +0300

    Second

--- LinkButtonUI/ContentView.swift
+++ LinkButtonUI/ContentView.swift
@@ -29,6 +29,22 @@ struct ContentView: View {
                 .buttonStyle(.plain)
                 .padding()
                 .border(.primary,width: 2)
+                .padding()
+            Link(destination: URL(string: "https://www.apple.com")!){
+                HStack(spacing: 16) {
+                    Image(systemName: "apple.logo")
+                    Text("Apple Store")
+                }
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(
+                    
+                    Capsule()
+                        .fill(Color.blue)
+                )
+            }
         }
         .padding()
     }
